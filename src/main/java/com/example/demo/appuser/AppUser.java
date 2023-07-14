9f5ac6baf489b91ca7f2a1dee03e9092f0826fc1
Ayhan Krala <codewolf27@gmail.com>
2023-07-14 15:30:02 +0300

    add files

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,5 +1,7 @@
 package com.example.demo.appuser;
 
+import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,26 +9,53 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class AppUser implements UserDetails {
+    @Id
+    @SequenceGenerator(name = "student_sequence",
+    sequenceName = "student_sequence",
+    allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                    generator = "student_sequence")
+
     private Long id ;
     private String name ;
-    private String Username ;
+    private String userName ;
     private String email ;
     private String password ;
-
-    private AppUser appUserRole ;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole ;
     private Boolean locked;
     private Boolean enabled;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority  =
-                new SimpleGrantedAuthority(appUserRole.name);
-        return Collections.singleton(authority);
+    public AppUser(String name,
+                   String userName,
+                   String email,
+                   String password,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+
+
+
     }
 
     @Override
-    public String getPassword() {
+    public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
 
@@ -37,21 +66,21 @@ public class AppUser implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return !locked;
+        return false;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return false;
     }
 
     @Override
     public boolean isEnabled() {
-        return enabled;
+        return false;
     }
 }
